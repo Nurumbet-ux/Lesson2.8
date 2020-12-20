15720f41c4e77ed05a61893efaa9cc960179c7b5
Nurumbet-ux <saryevnur@gmail.com>
2020-12-20 09:36:21 +0600

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,54 +5,42 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        NameComparator nameComparator = new NameComparator();
-        AgeComparator ageComparator = new AgeComparator();
-
-        Comparator<Person> comparator = nameComparator.thenComparing(ageComparator);
-
-        TreeSet<Person> people = new TreeSet<>(comparator);
-
-        people.add(new Person("Scarlet", 21));
-        people.add(new Person("Mirlan", 33));
-        people.add(new Person("Bill", 93));
-        people.add(new Person("Jack", 21));
-        people.add(new Person("Lucie", 54));
-        people.add(new Person("Robbin", 21));
-        people.add(new Person("Azamat", 21));
-        people.add(new Person("Azamat", 23));
-        people.add(new Person("Rob", 30));
-        people.add(new Person("Rob", 21));
-        System.out.println(people.size());
-
-        printList(people);
-
-        Person person = people.first();
-        System.out.println("First on the list: " + person.getName() + " " + person.getAge());
-        person = people.last();
-        System.out.println("Last on the list: " + person.getName() + " " + person.getAge());
-
-        person = new Person("Misha", 87);
-        people.add(person);
-        printList(people);
-
-        System.out.println("TailSet:");
-        printList(people.tailSet(person));
-
-        System.out.println("HeadSet:");
-        printList(people.headSet(person));
-
-        System.out.println("SubSet:");
-        printList(people.subSet(new Person("Azamat", 23), false, person, false));
-
-    }
-
-    public static void printList(Collection<Person> list) {
-        System.out.println("_______________________");
-        Iterator<Person> iterator = list.iterator();
+        HashMap<String, String[]> dictionary = new HashMap<>();
+        dictionary.put("dom", new String[]{"zhilishe", "hata", "berloga", "krysha"});
+        dictionary.put("prekrasnyi", new String[]{"zamechatelnyi", "voshititelnyi",
+                "shikarnyi"});
+        dictionary.put("chelovek", new String[]{"sapians", "persona", "individum", "lichnost"});
+        dictionary.put("bolshoi", new String[]{"ogromnyi", "gromadnyi", "velikiy"});
+        HashMap<String, String[]> mainDictionary = new HashMap<>(dictionary);
+        HashSet<String> keys = new HashSet<>(dictionary.keySet());
+        Iterator<String>iterator = keys.iterator();
         while (iterator.hasNext()) {
-            Person p = iterator.next();
-            System.out.println(p.getName() + " " + p.getAge());
+            String key = iterator.next();
+            int i =1;
+            while (i<dictionary.get(key).length){
+                ArrayList<String> arr = new ArrayList<>();
+                Collections.addAll(arr,dictionary.get(key));
+                String settingvalue = arr.get(i);
+                arr.set(i, key);
+                String[] newArr = new String[arr.size()];
+                int k = 1;
+                while (k< arr.size()){
+                    newArr[k] = arr.get(k);
+                    k++;
+                }
+                mainDictionary.put(settingvalue,newArr);
+                i++;
+            }
+
+
+            for (Map.Entry<String, String[]> item : mainDictionary.entrySet()) {
+                System.out.println(item.getKey() + " - " + Arrays.toString(item.getValue()));
+
+            }
+
+
         }
     }
 }
